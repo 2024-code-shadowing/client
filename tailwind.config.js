1ae7966dc8690f20ddb2972e7b80c3127424703c
jaykim0202 <jayjkim0223@gmail.com>
2024-03-21 23:13:52 +0900

    font family 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start-2p': ['"Press Start 2P"','system-ui'],
+      },
+      colors: {
+        'basic-blue': '#0000AA',
+        'hoverd-blue': '#02023C',
+        'clicked-blue': '#AAAAD9'
+      },
+    },
   },
   plugins: [],
 }
-

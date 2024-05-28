63c27543e684621727415e974922eb274351a656
jaykim0202 <jayjkim0223@gmail.com>
2024-05-28 23:06:54 +0900

    타이틀 페이지 API 연결 완료

--- src/TitleScreen.jsx
+++ src/TitleScreen.jsx
@@ -1,38 +1,31 @@
-import React, { useState } from "react";
-import PageRenderer from "./Background"
-
-const titleLines = [];
-
-for (let i = 1; i <= 50; i++) {
-  const title = `title ${i.toString().padStart(2, '0')}`;
-  const line = "xxx LINE";
-  const id = i;
-  titleLines.push({ title, line });
-}
+import React, { useState, useEffect } from "react";
+import PageRenderer from "./Background";
+import axios from "axios";
 
 const TitleLine = ({ title, line, id }) => (
   <div className="flex text-basic-blue gap-0 max-md:flex-wrap max-md:max-w-full">
-    <button className="flex-grow justify-center items-center px-16 py-5 text-2xl
+    <button
+      className="flex-grow justify-center items-center px-16 py-5 text-2xl
       border border-black border-solid bg-neutral-400 max-md:max-w-full
       hover:bg-btn-bg-red hover:text-btn-bg-yellow
       active:bg-btn-bg-yellowactive:text-btn-bg-red"
-      onClick={() => { window.location.href = `/typing/${id}`; }}>
+      onClick={() => {
+        window.location.href = `/typing/${id}`;
+      }}
+    >
       {title}
     </button>
     <div className="justify-center px-12 py-5 text-2xl
     border border-black border-solid bg-neutral-400 w-fit max-md:px-5">
-      {line}
+      {line} LINE
     </div>
-  </div >
+  </div>
 );
-
-
-const color_lt_gt = "active:text-white active:bg-black";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => (
   <div className="pagination-container w-full h-16 flex text-3xl items-center justify-center border border-black border-solid">
     <button
-      className={`${color_lt_gt} ms-2`}
+      className="active:text-white active:bg-black ms-2"
       onClick={() => onPageChange(currentPage - 1)}
       disabled={currentPage === 1}
     >
@@ -42,7 +35,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => (
       {currentPage} / {totalPages}
     </div>
     <button
-      className={`${color_lt_gt} me-2`}
+      className="active:text-white active:bg-black me-2"
       onClick={() => onPageChange(currentPage + 1)}
       disabled={currentPage === totalPages}
     >
@@ -51,16 +44,30 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => (
   </div>
 );
 
-const titleLinesPerPage = 8;
-const totalPages = Math.ceil(titleLines.length / titleLinesPerPage);
-
-function TitleScreen() {
+const TitleScreen = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [titleLines, setTitleLines] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchTitles = async () => {
+      try {
+        const response = await axios.get("/api/codes/titles");
+        setTitleLines(response.data.contents);
+        setTotalPages(Math.ceil(response.data.total / titleLinesPerPage));
+      } catch (error) {
+        console.error("Error fetching titles:", error);
+      }
+    };
+
+    fetchTitles();
+  }, []);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
 
+  const titleLinesPerPage = 8;
   const startIndex = (currentPage - 1) * titleLinesPerPage;
   const endIndex = startIndex + titleLinesPerPage;
   const visibleTitleLines = titleLines.slice(startIndex, endIndex);
@@ -71,8 +78,8 @@ function TitleScreen() {
         <PageRenderer page_type="go_back" />
       </div>
       <div>
-        {visibleTitleLines.map(({ title, line }) => (
-          <TitleLine key={title} title={title} line={line} />
+        {visibleTitleLines.map(({ title, line, codeId }) => (
+          <TitleLine key={codeId} title={title} line={line} id={codeId} />
         ))}
       </div>
       <div className="pagination-wrapper mt-auto">
@@ -84,6 +91,6 @@ function TitleScreen() {
       </div>
     </div>
   );
-}
+};
 
 export default TitleScreen;
